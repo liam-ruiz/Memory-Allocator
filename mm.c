b8d60eb98f58b8dbbeee36bbe1ccaba607c80ee9
Liam Ruiz-Steblein <ldr3@pyrite.clear.rice.edu>
2023-03-29 03:38:50 -0500

    :(

--- mm.c
+++ mm.c
@@ -43,6 +43,7 @@ struct pointer_data {
 	void *next;
 	void *prev;
 };
+typedef struct pointer_data *Pointers;
 
 /* Basic constants and macros: */
 #define WSIZE      sizeof(void *) /* Word and header/footer size (bytes) */
@@ -74,6 +75,8 @@ struct pointer_data {
 /* Global variables: */
 static char *heap_listp; /* Pointer to first block */  
 
+static struct pointer_data *dummy_head;
+
 /* Function prototypes for internal helper routines: */
 static void *coalesce(void *bp);
 static void *extend_heap(size_t words);
@@ -101,42 +104,51 @@ int
 mm_init(void) 
 {
 	
-	//struct pointer_data dummy_head;
-	//cant use this ^ need to cast 
+	void *bp;
 
 	/*creates the memory for the dummy head, prologue & epilogue*/
 	if ((heap_listp = mem_sbrk(5*WSIZE)) == (void *)-1)
 		return (-1);
+
+	dummy_head = (struct pointer_data *)heap_listp;
+	printf("dummy head: %p\n", dummy_head);
+	printf("lo heap: %p\n", mem_heap_lo());
 	// Put the address of the next node
-	PUT(heap_listp, &heap_listp + (sizeof(void *)));
+	PUT(heap_listp, (long)&heap_listp);
 	// Put the address of the prev node
-	PUT(heap_listp + (1 * WSIZE), &heap_listp);
+	PUT(heap_listp + (1 * WSIZE), (long)&heap_listp);
 	// Put prologue hdr, ftr, and epilogue hdr
 	PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); /* Prologue header */ 
 	PUT(heap_listp + (3 * WSIZE), PACK(DSIZE, 1)); /* Prologue footer */ 
-	PUT(heap_listp + (4 * WSIZE), PACK(0, 1))     /* Epilogue header */
+	PUT(heap_listp + (4 * WSIZE), PACK(0, 1));   /* Epilogue header */
+
+	printf("next adr: %p\n", heap_listp);
+	printf("prev adr: %p\n", (heap_listp + (1*WSIZE)));
+	printf("pro hdr adr: %p\n", (heap_listp + (2*WSIZE)));
+	printf("pro ftr adr: %p\n", ((heap_listp + (3*WSIZE))));
+	printf("ep hdr adr: %p\n", (heap_listp + (4*WSIZE)));
+	
+	printf("next: %p\n", dummy_head->next);
+	printf("prev: %p\n", dummy_head->prev);
+	printf("pro hdr: %u\n", *(heap_listp + (2*WSIZE)));
+	printf("pro ftr: %u\n", *((heap_listp + (3*WSIZE))));
+	printf("ep hdr: %u\n", *(heap_listp + (4*WSIZE)));
 
 	heap_listp += (3 * WSIZE);//Aligns the heap pointer between prologue hdr & ftr
 
-	dummy_head = *(struct pointer_data *)heap_listp;
-	
-	//*(void *)heap_listp = pointer_data->prev;
-	//wrong: *(uintptr_t *)heap_listp = ...);
-	
-	//bp = heap_listp; 
-	
-	
-	//need to cast to a bp? 
-	//not sure how to do this yet 
-
-
-	//TODO: Allocate space for struct, dummy head, connect dummy head to free block 
-
 	/* Extend the empty heap with a free block of CHUNKSIZE bytes. */
-	if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+	if ((bp = extend_heap(CHUNKSIZE / WSIZE)) == NULL)
 		return (-1);
 	
 	//insert the CHUNKSIZE block into the dummy head (explicit list)
+	insert_freeblock(bp, dummy_head);
+	printf("freeblock adr: %p\n", bp);
+	//bp = (struct pointer_data *)
+	printf("freeblock next: %u\n", *((int *)bp));
+	printf("freeblock prev: %u\n", *(((int *)bp) + 2));
+	printf("dummyhead next: %u\n", *((int *)(heap_listp - (3 * WSIZE))));
+	printf("dummyhead prev: %u\n", *((int *)(heap_listp - (2 * WSIZE))));
+	//printf("End of mm_init\n");
 	return (0);
 }
 
@@ -156,35 +168,36 @@ mm_malloc(size_t size)
 	size_t extendsize; /* Amount to extend heap if no fit */
 	void *bp;
 
+	printf("enters malloc\n");
+
 	/* Ignore spurious requests. */
 	if (size == 0)
 		return (NULL);
 
 	/* Adjust block size to include overhead and alignment reqs. */
-	if (size <= ALIGNMENT)
-		asize = ALIGNMENT + DSIZE;
+	if (size <= DSIZE) // Note, must be 4 words to holds hdrs & ftrs
+		asize = 2 * DSIZE;
 	else
 		asize = (ALIGNMENT * ((size + (ALIGNMENT - 1)) / ALIGNMENT)) + DSIZE;
-	
-
-	//TODO: EDIT FINDFIT
 
 	/* Search the free list for a fit. */
 	if ((bp = find_fit(asize)) != NULL) {
 		place(bp, asize);
+		// Remove from free list
+		remove_freeblock(bp);
 		return (bp);
 	}
 
 	/* No fit found.  Get more memory and place the block. */
-
-
+	
 	//Try coalescing here first? just a thought (w/ deffered coalescing) during 
 	//segragated explicit 
-
 	extendsize = MAX(asize, CHUNKSIZE);
 	if ((bp = extend_heap(extendsize / WSIZE)) == NULL)  
 		return (NULL);
 	place(bp, asize);
+	// Remove from free list
+	remove_freeblock(bp);
 	return (bp);
 } 
 
@@ -199,19 +212,21 @@ void
 mm_free(void *bp)
 {
 	size_t size;
+	void *blockP;
 
 	/* Ignore spurious requests. */
 	if (bp == NULL)
 		return;
 
-	//Edit so just frees the block (can do coalescing later)
+	//Edit so just frees the block (can do coalescing later?)
 	/* Free and coalesce the block. */
 	size = GET_SIZE(HDRP(bp));
 	PUT(HDRP(bp), PACK(size, 0));
 	PUT(FTRP(bp), PACK(size, 0));
 	
 	//Add block back into freelist
-	coalesce(bp);
+	blockP = coalesce(bp);
+	insert_freeblock(blockP, (heap_listp - (3 * WSIZE)));
 }
 
 /*
@@ -232,6 +247,8 @@ mm_realloc(void *ptr, size_t size)
 {
 	size_t oldsize;
 	void *newptr;
+
+	// TODO: fix for explicit list
 
 	/* If size == 0 then this is just free, and we return NULL. */
 	if (size == 0) {
@@ -285,21 +302,29 @@ coalesce(void *bp)
 
 	if (prev_alloc && next_alloc) {                 /* Case 1 */
 		return (bp);
-	} else if (prev_alloc && !next_alloc) {         /* Case 2 */
+	} else if (prev_alloc && !next_alloc) {         /* Case 2 - block after free */
 		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
 		PUT(HDRP(bp), PACK(size, 0));
 		PUT(FTRP(bp), PACK(size, 0));
-	} else if (!prev_alloc && next_alloc) {         /* Case 3 */
+		//removes block after bp from freelist 
+		remove_freeblock(NEXT_BLKP(bp));
+
+	} else if (!prev_alloc && next_alloc) {         /* Case 3 - block before free */
 		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
 		PUT(FTRP(bp), PACK(size, 0));
 		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
-	} else {                                        /* Case 4 */
+		//removes bp's block from freelist 
+		remove_freeblock(bp);
+	} else {                                        /* Case 4 - both before, after free*/
 		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
 		    GET_SIZE(FTRP(NEXT_BLKP(bp)));
 		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
 		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
+		//remove both bp, block after bp from freelist
+		remove_freeblock(NEXT_BLKP(bp));
+		remove_freeblock(bp);
 	}
 	return (bp);
 }
@@ -344,17 +369,25 @@ find_fit(size_t asize)
 {
 	void *bp;
 
-	void *bp2;
 	/*Change: search through free list */
 	
-	
-	(struct pointer_data *)bp2 = void *mem_heap_lo(void);
 	//bp2->next
 
 	/* Search for the first fit. */
-	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-		if (!GET_ALLOC(HDRP(bp)) && asize <= GET_SIZE(HDRP(bp)))
+	// for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+	// 	if (!GET_ALLOC(HDRP(bp)) && asize <= GET_SIZE(HDRP(bp)))
+	// 		return (bp);
+	// }
+	// Assign to dummy_head->next
+	bp = ((struct pointer_data *)(heap_listp - (3 * WSIZE)))->next;
+	printf("bp before: %lu\n", *((long *)bp));
+	//printf("hp before: %lu\n", *((long *)(heap_listp - (3*WSIZE))));
+	while (((char *)bp) != (heap_listp - (3 * WSIZE))) {
+		//printf("bp: %lu\n", *(long *)bp);
+		//printf("hp before: %lu\n", *((long *)(heap_listp - (3*WSIZE))));
+		if (asize <= GET_SIZE(HDRP(bp)))
 			return (bp);
+		bp = ((struct pointer_data *)&bp)->next;
 	}
 	/* No fit was found. */
 	return (NULL);
@@ -376,12 +409,13 @@ place(void *bp, size_t asize)
 
 	//TODO: EDIT FREELIST (if larger and needs to split)
 
-	if ((csize - asize) >= (2 * DSIZE)) { 
+	if ((csize - asize) >= (2 * DSIZE)) { // Large enough to split
 		PUT(HDRP(bp), PACK(asize, 1));
 		PUT(FTRP(bp), PACK(asize, 1));
 		bp = NEXT_BLKP(bp);
 		PUT(HDRP(bp), PACK(csize - asize, 0));
 		PUT(FTRP(bp), PACK(csize - asize, 0));
+		insert_freeblock(bp, (heap_listp - (3 * WSIZE)));
 	} else {
 		PUT(HDRP(bp), PACK(csize, 1));
 		PUT(FTRP(bp), PACK(csize, 1));
@@ -391,44 +425,43 @@ place(void *bp, size_t asize)
 
 /*
 * Requires:
-* 
+*   
 *
 * Effects: 
+*   Inserts bp after target in the free linked list. 
 *
 */
 static void
-insert_freeblock(void *bp,  void *where_head) 
+insert_freeblock(void *bp,  void *target) 
 {
-	struct pointer_data head_freelist;
-	struct pointer_data insert_freelist;
-	//IDK abt this tho:
-	head_freelist = (pointer_data *)where_head;
-	insert_freelist = (pointer_data *)bp;
-	//FIX TO FIFO 
-	head_freelist->next->prev = insert_freelist;
-	insert_freelist->next = head_freelist->next;
-	insert_freelist->prev = head_freelist;
-	head_freelist->next = insert_freelist;
+	//Casts to struct pointer_data * to use next and prev from the struct.
+
+	//sets the successor of target's prev to bp
+	((struct pointer_data *)
+	    &(((struct pointer_data *)&target)->next))->prev = bp;
+	((struct pointer_data *)&bp)->next = ((struct pointer_data *)&target)->next;
+	((struct pointer_data *)&bp)->prev = target;
+	((struct pointer_data *)&target)->next = bp;
 
 }
 /*
 * Requires:
 *
 * Effects: 
-*
+*   Removes bp from its free linked list. 
 */
 static void 
 remove_freeblock(void *bp)
 {
-	//Also IDK 
-	struct pointer_data remove_freelist;
+	//Casts to struct pointer_data * to use next and prev from the struct.
+	((struct pointer_data *)
+	    &(((struct pointer_data *)&bp)->prev))->next = ((struct pointer_data *)&bp)->next;
+	((struct pointer_data *)
+	    &(((struct pointer_data *)&bp)->next))->prev = ((struct pointer_data *)&bp)->prev;
 	
-	remove_freelist = (pointer_data *)bp;
-	remove_freelist->prev->next = remove_freelist->next;
-	remove_freelist->next->prev = remove_freelist->prev;
 }
 
-
+//Check that everything in freelist is actually free in checking routine 
 
 /* 
  * The remaining routines are heap consistency checker routines. 
